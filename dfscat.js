27c0213b96e8f85efdf5c54c452a662ee4901954
Paul Houghton <huffton@googlemail.com>
2019-09-05 22:13:04 +0100

    All the modes now work. Added detection based on load address.

--- dfscat.js
+++ dfscat.js
@@ -329,9 +329,10 @@ function displaySingleFile(catalogIndex)
 
   if (b) {
     $("#pbas").click();
-
   } else if (t) {
     $("#ptxt").click();
+  } else if (s) {
+    $("#pscr").click();
   } else if (d) {
     $("#pdis").click();
   } else {
@@ -1006,7 +1007,7 @@ function unpackTwoBitColor(packedInputData, screenMode, length)
   var buffer = new Uint8ClampedArray(modeInfo.height*modeInfo.width*4);
   var lineWidth = modeInfo.width;
 
-  for(var i = 0; i < length && i<modeInfo.height*modeInfo.width/(8/modeInfo.bits); i+=8 )
+  for(var i = 0; i < length && i<modeInfo.height*modeInfo.width*modeInfo.bits/8; i+=8 )
   {
     var charBytes = packedInputData.slice(i, i+8);
     // 8 rows of pixels dealt with at a time.
@@ -1039,28 +1040,65 @@ function unpackTwoBitColor(packedInputData, screenMode, length)
   return buffer;
 }
 
+function unpackFourBitColor(packedInputData, screenMode, length)
+{
+  var modeInfo = modeTable[screenMode];
+  var buffer = new Uint8ClampedArray(modeInfo.height*modeInfo.width*4);
+  var lineWidth = modeInfo.width;
+
+  for(var i = 0; i < length && i<modeInfo.height*modeInfo.width*modeInfo.bits/8; i+=8 )
+  {
+    var charBytes = packedInputData.slice(i, i+8);
+    // 8 rows of pixels dealt with at a time.
+    var lineNo=Math.trunc(i/(4*lineWidth));
+    var linePos=i-(lineWidth*lineNo*4);
+    // 8 bytes cover 4 columns
+    // 4 bytes per pixel 
+    // but i is incremented by 8, so reflected in linePos.
+    var destinationOffset = lineNo*8*lineWidth*4+linePos*4/4;
+    //console.log("I: "+i+"  Dest: "+destinationOffset);
+
+    var row1 = decodeFourBitByte(charBytes, 0, modeInfo);
+    var row2 = decodeFourBitByte(charBytes, 1, modeInfo);
+    var row3 = decodeFourBitByte(charBytes, 2, modeInfo);
+    var row4 = decodeFourBitByte(charBytes, 3, modeInfo);
+    var row5 = decodeFourBitByte(charBytes, 4, modeInfo);
+    var row6 = decodeFourBitByte(charBytes, 5, modeInfo);
+    var row7 = decodeFourBitByte(charBytes, 6, modeInfo);
+    var row8 = decodeFourBitByte(charBytes, 7, modeInfo);
+     
+    buffer.set(row1, (lineWidth * 4 * 0) + destinationOffset);
+    buffer.set(row2, (lineWidth * 4 * 1) + destinationOffset);
+    buffer.set(row3, (lineWidth * 4 * 2) + destinationOffset);
+    buffer.set(row4, (lineWidth * 4 * 3) + destinationOffset);
+    buffer.set(row5, (lineWidth * 4 * 4) + destinationOffset);
+    buffer.set(row6, (lineWidth * 4 * 5) + destinationOffset);
+    buffer.set(row7, (lineWidth * 4 * 6) + destinationOffset);
+    buffer.set(row8, (lineWidth * 4 * 7) + destinationOffset);
+  }
+  return buffer;
+}
+
 function d_scr(catalogIndex)
 {
   var fileItem = catalog[catalogIndex];
   var startOffset = fileItem.startSector * 256; // DFS Disks have 256 bytes per sector
   var fileData = new Uint8Array(binaryDiskBlob, startOffset, fileItem.fileLength);
   var canvas, ctx;
+  var buffer = "";
 
   for (mode in modeTable) {
-    console.log(JSON.stringify(mode));
-    buffer = "";
     switch (modeTable[mode].bits)
     {
       case 1:
         buffer = unpackOneBitColor(fileData, mode, fileItem.fileLength);
         break;
       case 2:
-        console.log('Mode: '+mode);
         buffer = unpackTwoBitColor(fileData, mode, fileItem.fileLength);
         break;
-//      case 4:
-//        unpackFourBitColor(fileData, mode);
-//        break;
+      case 4:
+        buffer = unpackFourBitColor(fileData, mode, fileItem.fileLength);
+        break;
     }
     canvas = document.getElementById('beebScreen'+mode);
     ctx = canvas.getContext("2d");
@@ -1071,5 +1109,10 @@ function d_scr(catalogIndex)
     var idata = ctx.createImageData(modeTable[mode].width, modeTable[mode].height);
     idata.data.set(buffer.slice(0,4*modeTable[mode].width*modeTable[mode].height));
     ctx.putImageData(idata, 0, 0);
+  }
+  if ( catalog[catalogIndex].fileLoad >= 12288 && catalog[catalogIndex].fileLoad + catalog[catalogIndex].fileLength <= 32768 ) {
+    return true;
+  } else { 
+    return false;
   }
 }
